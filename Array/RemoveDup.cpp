92534f0caa79f1dd78c5e59633711132877fc4da
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-05-11 19:30:01 +0530

    Remove Duplicates

--- Array/RemoveDup.cpp
+++ Array/RemoveDup.cpp
@@ -2,25 +2,46 @@
 
 using namespace std;
 
-void remdup(int arr[], int n){
-    int newarr[n];
-    for (int i =0; i< n; i++){
-        
+bool isavail(int arr[], int n, int a){
+    for (int i =0; i<n; i++){
+        if (arr[i] == a){
+            return true;
+        }
     }
+    return false;
+}
+
+int remdup(int arr[], int n){
+    int newarr[n];
+    int r = 0;
+    for (int i =0; i< n; i++){
+        if (isavail(newarr, r, arr[i])){
+            continue;
+        }
+        newarr[r] = arr[i];
+        r++;
+    }
+
+    for (int j = 0; j<r; j++){
+        arr[j] = newarr[j];
+    }
+
+    return r-1;
 }
 
 int main() {
-    int n;
-    cout<<"Enter number of elements: ";
-    cin>>n;
-    int arr[n];
-    for (int i=0; i<n; i++){
-        int num;
-        cout<<"Enter element "<< i+1<<": "<<endl;
-        cin>>num;
-        arr[i] = num;
-    }
+    
+    int arr[] = {0,0,0,1,1,1,1,2,2,3,3,3,4,4,4};
+    int n = sizeof(arr)/sizeof(int);
+    int r = remdup(arr,n);
 
-    remdup(arr,n);
+    for (int i=0; i< n; i++){
+        if(i<=r){
+            cout<< arr[i] << " ";
+        }
+        else{
+            cout<< "_" << " ";
+        }
+    }cout<<"\n";
     return 0;
 }
