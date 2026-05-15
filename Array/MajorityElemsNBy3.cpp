f0c33adc9c56f1120e4acef30d53be0b5e11d0e2
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-05-15 22:01:00 +0530

    Majority Elements Array count greater than N/3

---
+++ Array/MajorityElemsNBy3.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void findMaj(int arr[], int n){
+    vector<int> nums;
+    int thres = n/3;
+    int count = 1;
+    for (int i = 0; i<n; i++){
+        for (int j = 0; j < n; j++){
+            if (arr[i] == arr[j] && i != j){
+                count++;
+            }
+        }
+        if (count >= thres && !(find(nums.begin(),nums.end(),arr[i]) != nums.end())){
+            nums.push_back(arr[i]);
+        }
+        count = 1;
+    }
+    if (nums.size()>0){
+        cout << "[ ";
+        for (int i = 0; i<nums.size(); i++){
+            cout << nums[i] << " ";
+        }cout << "]" << endl;
+    }
+    else{
+        cout << "No majority element\n";
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 1, 1, 3, 2, 2, 3, 3};
+    int n = sizeof(arr)/sizeof(int);
+
+    findMaj(arr,n);
+    return 0;
+}
