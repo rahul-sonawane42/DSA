2ac8b17a5c3a39c2eb304e3400eb28941bd44d5c
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-05-21 20:51:44 +0530

    Lower Bound

---
+++ Binary Search/LowerBound.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int lowerb(int arr[], int n, int x){
+    int s = 0;
+    int e = n-1;
+    int num = n;
+
+    while (s<=e){ 
+        int m = s + (e-s)/2;
+        
+        if (arr[m] >= x){
+            num = m;
+            e = m-1;
+        } else {
+            s = m+1;
+        }
+    }
+    return num;
+}
+
+int main() {
+    int arr[] = {3,5,8,15,19};
+    int n = sizeof(arr)/sizeof(int);
+    int x = 9;
+    cout << "Lower Bound: " << lowerb(arr,n,x) << endl;
+    return 0;
+}
