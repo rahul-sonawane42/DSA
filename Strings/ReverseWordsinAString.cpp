b81f4c52f9b558ec81270e6554b07746c9e3700a
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-06-06 01:11:54 +0530

    Reverse Words in a String

---
+++ Strings/ReverseWordsinAString.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string reverseWords(string s){
+    string result = "";
+    int i = s.size()-1;
+
+    while (i>=0){
+        while (i>=0 && s[i]== ' '){
+            i--;
+        }
+
+        if (i<0) break;
+
+        int end = i;
+
+        while (i>=0 && s[i]!= ' '){
+            i--;
+        }
+
+        string word = s.substr(i+1, end - i);
+
+        if (!result.empty()){
+            result += " ";
+        }
+
+        result += word;
+    }
+    return result;
+}
+
+int main() {
+    string s = " amazing coding skills ";
+    cout << reverseWords(s) << endl;
+    return 0;
+}
