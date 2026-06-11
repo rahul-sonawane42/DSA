9d8908b32c9bbedc0d4f3db143e8ef893bc04a15
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-06-11 22:58:46 +0530

    Added Optimized Code

--- Strings/RotateString.cpp
+++ Strings/RotateString.cpp
@@ -17,11 +17,30 @@ bool isrotation(string s, string f){
     return false;
 }
 
+bool optimizedisrotation(string s, string f){
+    s = s+s;
+    int n = s.size();
+    int start = 0;
+    int end = f.size()-1;
+    while (end<n){
+        if (s.substr(start, end-start+1) == f) return true;
+        start++;
+        end++;
+    }
+    return false;
+}
+
 int main() {
     string s = "rotation";
     string f = "tionrota";
 
     if (isrotation(s,f)){
+        cout << "Rotated" << endl;
+    } else {
+        cout << "Not Rotated" << endl;
+    }
+
+    if (optimizedisrotation(s,f)){
         cout << "Rotated" << endl;
     } else {
         cout << "Not Rotated" << endl;
