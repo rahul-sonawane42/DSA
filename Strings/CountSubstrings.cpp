4bfe64d7ea1243ce275c4adf472ad53a3f33b5db
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-06-17 22:31:08 +0530

    Added Optimised Code

--- Strings/CountSubstrings.cpp
+++ Strings/CountSubstrings.cpp
@@ -24,11 +24,34 @@ int countSubstr(string s, int k){
     return count;
 }
 
+int atMostDistinct(string s, int k){
+    int left = 0, res = 0;
+    unordered_map<char, int> freq;
+
+    for (int right = 0; right<s.size(); right++){
+        freq[s[right]]++;
+
+        while (freq.size()>k){
+            freq[s[left]]--;
+            if (freq[s[left]] == 0) freq.erase(s[left]);
+            left++;
+        }
+        res += (right-left+1);
+    }
+    return res;
+}
+
+int optCountSubstr(string s, int k){
+    return atMostDistinct(s,k) - atMostDistinct(s,k-1);
+}
+
 int main() {
     
     string str = "pqpqs";
     int k = 2;
-    int result = countSubstr(str,k);
-    cout << result <<endl;
+    int result1 = countSubstr(str,k);
+    cout << result1 <<endl;
+    int result2 = optCountSubstr(str,k);
+    cout << result2 <<endl;
     return 0;
 }
