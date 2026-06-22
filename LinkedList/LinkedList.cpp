8028a7dc0a11868bb97920e1fe8cb1d450645dcb
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-06-22 23:11:05 +0530

    Search for a number in a LL

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -40,6 +40,30 @@ class Solution{
             return head;
         }
 
+        int listLen(Node* head){
+            if (head == NULL){
+                return 0;
+            }
+            int len = 0;
+            Node* temp = head;
+            while (temp != NULL){
+                len++;
+                temp = temp->next;
+            }
+            return len;
+        }
+
+        bool search(Node* head, int val){
+            Node* temp = head;
+            while (temp != NULL){
+                if (temp->data == val){
+                    return true;
+                }
+                temp = temp->next;
+            }
+            return false;
+        }
+
         void printList(Node* head){
             Node* temp = head;
             while (temp != NULL){
@@ -56,17 +80,42 @@ int main() {
     Node* head = new Node(5);
     head->next = new Node(3);
 
+    cout << "------------------------------------------" << endl;
+    
     cout << "Original List" << endl;
     sol.printList(head);
-
+    cout << "Length: " << sol.listLen(head) << endl;
+    
+    cout << "------------------------------------------" << endl;
+    
     head = sol.insertAtHead(head,2);
-
     cout << "After Insertion" << endl;
     sol.printList(head);
-
+    cout << "Length: " << sol.listLen(head) << endl;
+    
+    cout << "------------------------------------------" << endl;
+    
     head = sol.deleteLast(head);
     cout << "After Deletion" << endl;
     sol.printList(head);
+    cout << "Length: " << sol.listLen(head) << endl;
+    
+    cout << "------------------------------------------" << endl;
+    cout << "Search 5 in the list: ";
+    if (sol.search(head,5)){
+        cout << "Found" << endl;
+    } else {
+        cout << "Not Found" << endl;
+    }
 
+    cout << "------------------------------------------" << endl;
+    cout << "Search 6 in the list: ";
+    if (sol.search(head,6)){
+        cout << "Found" << endl;
+    } else {
+        cout << "Not Found" << endl;
+    }
+    
+    cout << "------------------------------------------" << endl;
     return 0;
 }
