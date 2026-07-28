a0f31774f4b14e3e142dea6224e8d7244fe89ecc
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-07-28 23:40:44 +0530

    FInd pairs with given sum

---
+++ LinkedList/Doubly/pairsWithSum.cpp
@@ -0,0 +1,3 @@
+// Problem Statement: Given the head of a sorted doubly linked list of positive distinct integers, and a target integer, return a 2D array containing all unique pairs of nodes (a, b) such that a + b == target.
+
+// Each pair should be returned as a 2-element array [a, b] with a < b. The list is sorted in ascending order. If there are no such pairs, return an empty list.
